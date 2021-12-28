de7921852a03c4e625363785ed5110e4bf2d2ed5
Nik <sliverregulyar@gmail.com>
2021-12-28 16:15:13 +0300

    Second HW

--- script.js
+++ script.js
@@ -1,22 +1,58 @@
-let numbersOfFilms=+prompt("Numbers of films");
+"use strict";
 
-const personalMovieDB={
-    count:numbersOfFilms,
-    movies:{},
-    actors:{},
-    genres:[],
-    privat:false
+
+//First HW
+// let numbersOfFilms=+prompt("Numbers of films");
+
+const personalMovieDB = {
+    count: +prompt("Numbers of films"),
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+    addMovie: function (movie = prompt("What a film you look")) {
+        this.movies[movie] = (movie == "Jay and Silent Bob") ? 9999 : prompt("Set score for him");
+    },
+    addMovies:function(){
+        if(+this.count>0){
+            
+            for(let i=0;i<this.count;i++){
+                console.log(movie);
+            }
+        }
+    }
+
 }
 
-let i=0;
-while(i<personalMovieDB.count){3
-    i++;
-    let movie=prompt("What a film you look"),
-        score=prompt("Set score for him");
-    personalMovieDB.movies[movie]=score;
+//first variant
+// let i=0;
+// while(i<personalMovieDB.count){3
+//     i++;
+//     let movie=prompt("What a film you look"),
+//         score=prompt("Set score for him");
+//     personalMovieDB.movies[movie]=score;
+// }
+
+//second
+
+// for(let i=0; i <  personalMovieDB.count;i++){
+
+//     let movie=prompt("What a film you look"),
+//         score=prompt("Set score for him");
+//     personalMovieDB.movies[movie]=score;
+// }
+
+//third
+for (let i = personalMovieDB.count; i != 0; i--) {
+    if (i == 3) {
+        personalMovieDB.addMovie("Jay and Silent Bob");
+        continue;
+    }
+    personalMovieDB.addMovie();
+
 }
 
-
+// Fourth
 
 console.log(personalMovieDB);
 
